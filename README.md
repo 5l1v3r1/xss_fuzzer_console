a55a0d0d042a37476415e5969821bf20897d416d
tytrusty <tytrusty@utexas.edu>
2017-01-07 06:51:34 -0600

    Fuzzing parameter changing

--- README.md
+++ README.md
@@ -1,31 +1,35 @@
 # Ty's XSS Fuzzer
 
 Note0: Still a WIP <br>
-Note1: This is my first python program so the code may not be high quality stuff
+Note1: This is my first python program so the code may not be high quality stuff <br>
 
-Semi-intelligent fuzzing tool. Program analyzes html output and constructs attack strings based on the output. If desired, the program will modify each character to attempt to bypass filtering. 
+Semi-intelligent fuzzing tool. Program analyzes html output and constructs attack strings based on the output. If desired, the program will modify each character to attempt to bypass filtering. <br>
 
-One advantage of this fine-tuned approach is fewer network connections, so you don't have to flood the target network. 
+One advantage of this fine-tuned approach is fewer network connections, so you don't have to flood the target network. <br>
 
-Program interface is a unix-like command prompt (Think msfconsole).
+Program interface is a unix-like command prompt (Think msfconsole). <br>
 
-Multithreaded Support - Set thread count when spidering or fuzzing
+Multithreaded Support - Set thread count when spidering or fuzzing. <br>
 
-Connection Delay - Set connection delays to avoid flooding the target or to avoid any mechanisms that prevent frequent connections.
+Connection Delay - Set connection delays to avoid flooding the target or to avoid any mechanisms that prevent frequent connections.<br>
 
 ##Todo: Additions
 
 ###Large 
-Stored XSS analysis
+Stored XSS analysis<br>
+Right now only the spider has been implemented; initial version of fuzzer not added yet <br>
 
 ###Medium 
-Handle JS reflect context
-Handle unique attribute reflect content
-Improve Spider: Consider beautifulsoup, check 'src' attr
-Check forms/input for URLs (currently only check a tags)
+Handle JS reflect context<br>
+Handle unique attribute reflect content<br>
+Improve Spider: Consider beautifulsoup, check 'src' attr<br>
+Check forms/input for URLs (currently only check a tags)<br>
+Make attack on main thread with a whole lot of progress output <br>
 
 ###Small 
-Spider output when finished 
-Handle printing to sdout without disrupting the cmd line
+Spider output when finished <br>
+Handle printing to sdout without disrupting the cmd line<br>
+Implement show command to show current attack urls <br>
+For showing specific url data show snippet of reflection and current fuzzer characters, and the target string<br>
 
 
--- attack.py
+++ attack.py
@@ -7,10 +7,14 @@ import random
 import string
 import util
 
+# TODO ONE PARAM AT A TIME ... This is important because one param
+# may cause 500 error if some critical boolean value is change dto cookie
+
 # Class containing necessary metadata for each attack vector
 class AttackContext:
     # Context essentially refers to the attack location 
     queue = None       # Make DictQueue object
+    parse_url = None   # Parsed URL object
     cookie = ''        # Inserted cookie to find
     data = ''          # Raw HTML
 
@@ -34,33 +38,28 @@ class AttackContext:
     f = OrderedDict()
 
     # Initialize class variables and parse current context
-    def __init__(self, data, cookie, pos):
+    def __init__(self, queue, parsed_url, data, cookie, pos):
+        self.queue = queue
+        self.parsed_url = parsed_url
         self.data = data
         self.cookie = cookie
         self.key = gen_key()
-
-
         d = 0 # Distance from initial pos
+
         for i in reversed(data[0:pos]):
             d += 1
-            
             if i == '>': # Parent tag is closed
                 self.tag_closed = True
-            
             elif i == '<': # Look complete, because tag found
                 self.set_tag(pos-d+1, pos)
                 break
-            
             elif i == '{': # JavaScript context
                 self.is_js = True
-
             elif i == '=': # Value assignment complete
                 self.in_value = False
-
             # Checking value delimiter
             elif i == '\'' or i == '\"' and self.in_value:
                 self.delimiter = i
-
         self.set_target_chars() # Identify fuzzer characters
         self.make_fuzz_str()    # Construct fuzzer string
 
@@ -83,39 +82,52 @@ class AttackContext:
         
         if self.tag_closed: # Inside of tag content section
             # Need chars: < / > script
-            self.f.update({'<':('<',
+            self.f.update({'<':'<',
                            '/':'/',
                            '>':'<',
                            'script':'script'})
         else: # Desired objective is to introduce new attr
             # TODO for these you don't need to escape completely, you
             # can instead just introduce a new attribute like onerror
+            self.f.update({'<':'<',
+                           '/':'/',
+                           '>':'<',
+                           'script':'script'})
 
         if self.is_js:
             self.f.update({';':';'})
             print 'JS_CONTEXT --set_target_chars'
     
+    # Construct a fuzzing string to account for filtering
     def make_fuzz_str(self):
         fuzz = self.cookie 
-        print 'fuzz begin ' + fuzz
         for char in self.f.values():
             fuzz += self.key
             fuzz += char
+        print 'fuzz ' + fuzz
+        print gen_url(self.parsed_url, fuzz)
 
+    # Construct a functional string to introduce an alert script
+    def make_atk_str(self):
+        pass
+
+    # Fuzz the context using the fuzzer string
     def fuzz_context(self):
-        
+        pass
 
 # Class containing Attack URLs with their associated metadata
 class AttackURL:
     queue = None # DictQueue object
+    parsed_url = None
     cookie = str(binascii.b2a_hex(os.urandom(2))) # Generate cookie
     url = ''
     visited = False
     data = '' # html data
     atk_vectors = list() # attack vectors -- list of AttackContext's
 
-    def __init__(self, queue, url):
+    def __init__(self, queue, parsed_url, url):
         self.queue = queue
+        self.parsed_url = parsed_url
         self.url = url
 
     def set_data(self, data):
@@ -125,24 +137,20 @@ class AttackURL:
     def init_context(self):
         if self.data == '':
             return None
-
+        
         # Find all cookie reflections in the HTML
         match = util.string_match(self.data, self.cookie)
         for pos in match:
-            context = AttackContext(self.data, self.cookie, pos)
+            context = AttackContext(self.queue, self.parsed_url,
+                                    self.data, self.cookie, pos)
 
     # Generates an attack object for parameterized URLs
     @staticmethod
     def create(queue, parsed_url, params):
         p = parsed_url
         # Changing each argument value to the cookie
-        for param, value in params.items(): 
-            params[param] = AttackURL.cookie
-        new_params = urlencode(params, doseq=True)
-        new_url = ParseResult(p.scheme, p.netloc, p.path, p.params,
-                new_params, p.fragment).geturl()
-
-        return AttackURL(queue, new_url) # Return new attack object
+        new_url = gen_url(p, AttackURL.cookie) 
+        return AttackURL(queue, p, new_url) # Return new attack object
 
     def attack(self):
         pass
@@ -150,3 +158,15 @@ class AttackURL:
 # Generate random two character key
 def gen_key():
     return ''.join(random.choice(string.ascii_uppercase) for _ in range(3))
+
+# Generate URL with custom query values
+def gen_url(p, value):
+    params = parse_qs(p.query.encode('utf-8')) 
+    for param in params.keys(): 
+        params[param] = value
+    new_params = urlencode(params, doseq=True)
+    print str(new_params)
+    return ParseResult(p.scheme, p.netloc, p.path, p.params,
+                       new_params, p.fragment).geturl()
+
+
