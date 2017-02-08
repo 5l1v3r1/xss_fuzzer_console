d70a1d9d237035b2cbc6466518a237b8cc81c590
tytrusty <tytrusty@utexas.edu>
2017-02-08 16:24:18 -0600

    Modified docs

--- docs/_build/html/_sources/index.rst.txt
+++ docs/_build/html/_sources/index.rst.txt
@@ -1,5 +1,5 @@
 .. XSS Shell documentation master file, created by
-   sphinx-quickstart on Wed Feb  8 16:09:09 2017.
+   sphinx-quickstart on Wed Feb  8 16:23:59 2017.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # XSS Shell documentation build configuration file, created by
-# sphinx-quickstart on Wed Feb  8 16:09:09 2017.
+# sphinx-quickstart on Wed Feb  8 16:23:59 2017.
 #
 # This file is execfile()d with the current directory set to its
 # containing dir.
@@ -30,7 +30,7 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = ['sphinx.ext.autodoc']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
